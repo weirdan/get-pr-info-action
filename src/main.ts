773f646173f4cbc2a45d24bc6b573e2a1c6b3d11
Bruce Weirdan <weirdan@gmail.com>
2020-08-14 21:49:33 +0300

    Initial implementation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
+    const token = core.getInput('token', {required: true})
+    const pullNumber = parseInt(core.getInput('pull_number', {required: true}))
+    const result = await github.getOctokit(token, {}).pulls.get({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: pullNumber
+    })
+    core.setOutput('pull_request', result.data)
   } catch (error) {
     core.setFailed(error.message)
   }
